2da6a7a663aca269cb2557589c9ea9d8ef9fa0e0
hl3470 <hl3470@naver.com>
2024-05-16 23:10:08 +0900

    feat: BaseResponse 구현 및 Error,DataResponse생성

---
+++ backend/src/main/java/middle_point_search/backend/common/dto/BaseResponse.java
@@ -0,0 +1,22 @@
+package middle_point_search.backend.common.dto;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatusCode;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Getter;
+
+@Getter
+public class BaseResponse {
+	@JsonFormat(pattern = "yyyy-MM-dd kk:mm:ss")
+	private final LocalDateTime timestamp = LocalDateTime.now();
+	private final boolean isSuccess;
+	private final HttpStatusCode status;
+
+	protected BaseResponse(boolean isSuccess, HttpStatusCode status) {
+		this.isSuccess = isSuccess;
+		this.status = status;
+	}
+}
