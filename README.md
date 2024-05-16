0a25d14aa1f80b1de5512e2c43e73c904ede3898
hl3470 <hl3470@naver.com>
2024-05-16 22:51:56 +0900

    init project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Middle_Point-Search-Backend
+중간지점찾기 프로젝트 백엔드 레포지토리입니다.
---
+++ backend/src/test/java/middle_point_search/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package middle_point_search.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
