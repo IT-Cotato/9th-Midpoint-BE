ea076bf1cc09c4c225d406793429d939895aebb1
hl3470 <hl3470@naver.com>
2024-05-21 16:32:47 +0900

    refactor: ErrorResponse create 메서드 변경
    create메서드를 제거하고 of, from 메서드를 추가하였다.

--- backend/src/main/java/middle_point_search/backend/common/dto/ErrorResponse.java
+++ backend/src/main/java/middle_point_search/backend/common/dto/ErrorResponse.java
@@ -1,22 +1,34 @@
 package middle_point_search.backend.common.dto;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import lombok.Getter;
+import middle_point_search.backend.common.exception.CustomException;
+import middle_point_search.backend.common.exception.errorCode.ErrorCode;
 
 @Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorResponse extends BaseResponse {
-	private final String error;
 
 	private ErrorResponse(boolean isSuccess, HttpStatusCode status, String error) {
 		super(isSuccess, status);
 		this.error = error;
 	}
 
-	public ErrorResponse create(boolean isSuccess, HttpStatusCode status, String error) {
+	public static ErrorResponse of(HttpStatusCode status, String error) {
+		boolean isSuccess = false;
+
+		return new ErrorResponse(isSuccess, status, error);
+	}
+
+	public static ErrorResponse from(ErrorCode errorCode) {
+		boolean isSuccess = false;
+		HttpStatus status = errorCode.getHttpStatus();
+		String error = errorCode.getMessage();
+
 		return new ErrorResponse(isSuccess, status, error);
 	}
 
