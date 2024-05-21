74c50d6bd1bee5d3e0d1246c0d7caeadcd385638
hl3470 <hl3470@naver.com>
2024-05-21 16:40:26 +0900

    refactor: ErrorResponse에 error 필드 사라진 문제 수정

--- backend/src/main/java/middle_point_search/backend/common/dto/ErrorResponse.java
+++ backend/src/main/java/middle_point_search/backend/common/dto/ErrorResponse.java
@@ -6,12 +6,12 @@ import org.springframework.http.HttpStatusCode;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import lombok.Getter;
-import middle_point_search.backend.common.exception.CustomException;
 import middle_point_search.backend.common.exception.errorCode.ErrorCode;
 
 @Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorResponse extends BaseResponse {
+	private final String error;
 
 	private ErrorResponse(boolean isSuccess, HttpStatusCode status, String error) {
 		super(isSuccess, status);
