7c83332b54117095e61f97bfb92ac45e294ecf02
hl3470 <hl3470@naver.com>
2024-05-28 15:44:41 +0900

    refactor: 에러메세지 변경

--- backend/src/main/java/middle_point_search/backend/common/exception/errorCode/CommonErrorCode.java
+++ backend/src/main/java/middle_point_search/backend/common/exception/errorCode/CommonErrorCode.java
@@ -7,10 +7,10 @@ import lombok.Getter;
 @Getter
 public enum CommonErrorCode implements ErrorCode {
 
-	INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "Invalid parameter included"),
-	RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "Resource not exists"),
-	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
-	DATA_BUFFER_LIMIT_ERROR(HttpStatus.PAYLOAD_TOO_LARGE, "Data buffer limit error"),
+	INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+	RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "요청 자원이 존재하지 않습니다."),
+	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부에서 에러가 발생하였습니다."),
+	DATA_BUFFER_LIMIT_ERROR(HttpStatus.PAYLOAD_TOO_LARGE, "요청의 페이로드가 너무 큽니다."),
 	;
 
 	private final HttpStatus httpStatus;
