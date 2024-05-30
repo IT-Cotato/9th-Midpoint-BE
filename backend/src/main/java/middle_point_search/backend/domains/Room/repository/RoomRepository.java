41f2c13a99b8eace7fa0b803803d31a05354b7b9
hl3470 <hl3470@naver.com>
2024-05-30 20:13:18 +0900

    chore: 디렉터리명 Room 에서 room으로 변경

--- backend/src/main/java/middle_point_search/backend/domains/Room/repository/RoomRepository.java
+++ backend/src/main/java/middle_point_search/backend/domains/Room/repository/RoomRepository.java
@@ -1,8 +1,8 @@
-package middle_point_search.backend.domains.Room.repository;
+package middle_point_search.backend.domains.room.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import middle_point_search.backend.domains.Room.domain.Room;
+import middle_point_search.backend.domains.room.domain.Room;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
 
