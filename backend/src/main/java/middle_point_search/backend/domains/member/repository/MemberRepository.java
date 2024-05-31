aa2a00b914eea90247d846fed35c429e3d48777b
hl3470 <hl3470@naver.com>
2024-05-31 20:38:51 +0900

    feat: MemberRepository 구현

---
+++ backend/src/main/java/middle_point_search/backend/domains/member/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package middle_point_search.backend.domains.member.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import middle_point_search.backend.domains.member.domain.Member;
+import middle_point_search.backend.domains.room.domain.Room;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+	Optional<Member> findByRoomAndName(Room room, String name);
+}
