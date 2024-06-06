f22fadda374199501a45b2869c4e4931967cf7f0
hl3470 <hl3470@naver.com>
2024-06-06 15:31:23 +0900

    feat: JwtProperties 구현
    Jwt 관련 상수들을 일괄적으로 관리하기 위해 JwtProperties 구현

---
+++ backend/src/main/java/middle_point_search/backend/common/security/jwt/constants/JwtProperties.java
@@ -0,0 +1,40 @@
+package middle_point_search.backend.common.security.jwt.constants;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@Component
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProperties {
+
+	private String issuer;
+	private String secret;
+	private Access access;
+	private Refresh refresh;
+
+	private final String ACCESS_TOKEN_SUBJECT = "AccessToken";
+	private final String REFRESH_TOKEN_SUBJECT = "RefreshToken";
+	private final String NAME_CLAIM = "id";
+	private final String ROOM_ID_CLAIM = "roomId";
+	private final String BEARER = "Bearer ";
+
+	@Getter
+	@RequiredArgsConstructor
+	public static class Access {
+		private int expiration;
+		private String header;
+	}
+
+	@Getter
+	@RequiredArgsConstructor
+	public static class Refresh {
+		private int expiration;
+		private String header;
+	}
+}
