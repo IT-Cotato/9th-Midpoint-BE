aab0886891186f9a6fd984b60b75300e7c2ea65c
hl3470 <hl3470@naver.com>
2024-06-10 20:24:54 +0900

    refactor: CustomUsernamePasswordAuthenticationToken.of(user); 에서 from으로 변경

--- backend/src/main/java/middle_point_search/backend/common/security/login/provider/CustomAuthenticationProvider.java
+++ backend/src/main/java/middle_point_search/backend/common/security/login/provider/CustomAuthenticationProvider.java
@@ -1,14 +1,13 @@
 package middle_point_search.backend.common.security.login.provider;
 
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.User;
 
 import lombok.RequiredArgsConstructor;
 import middle_point_search.backend.common.exception.CustomException;
 import middle_point_search.backend.common.exception.errorCode.UserErrorCode;
+import middle_point_search.backend.common.security.dto.CustomUserDetails;
 import middle_point_search.backend.common.security.dto.CustomUserDetailsImpl;
 import middle_point_search.backend.common.security.dto.CustomUsernamePasswordAuthenticationToken;
 import middle_point_search.backend.common.security.login.service.CustomUserDetailsService;
@@ -21,18 +20,19 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 	@Override
 	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
 
-		String username=(String) authentication.getPrincipal();
-		String password=(String) authentication.getCredentials();
-		String placeRoomId = ((CustomUsernamePasswordAuthenticationToken) authentication).getPlaceRoomId();
+		String username = (String)authentication.getPrincipal();
+		String password = (String)authentication.getCredentials();
+		String placeRoomId = ((CustomUsernamePasswordAuthenticationToken)authentication).getPlaceRoomId();
 
-		CustomUserDetailsImpl user = (CustomUserDetailsImpl)userDetailsService.loadUserByUsernameAndRoomId(username, placeRoomId);
+		CustomUserDetails user = (CustomUserDetailsImpl)userDetailsService.loadUserByUsernameAndRoomId(username,
+			placeRoomId);
 
 		// 비밀번호 확인
-		if(!matchPassword(password, user.getPassword())) {
+		if (!matchPassword(password, user.getPassword())) {
 			throw new CustomException(UserErrorCode.MEMBER_CREDENTIAL_UNAUTHORIZED);
 		}
 
-		return CustomUsernamePasswordAuthenticationToken.of(username,password, user.getRoomId());
+		return CustomUsernamePasswordAuthenticationToken.from(user);
 	}
 
 	@Override
