2b9bcc4e5430c5ca1056cfd3db0e9d07280fbeba
hl3470 <hl3470@naver.com>
2024-06-11 22:22:33 +0900

    feat: filter체인에서 발생하는 예외를 처리하기 위한 ExceptionHandlingFilter 구현

---
+++ backend/src/main/java/middle_point_search/backend/common/security/exception/hadlingFilter/ExceptionHandlingFilter.java
@@ -0,0 +1,37 @@
+package middle_point_search.backend.common.security.exception.hadlingFilter;
+
+import java.io.IOException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import middle_point_search.backend.common.dto.DataResponse;
+import middle_point_search.backend.common.dto.ErrorResponse;
+import middle_point_search.backend.common.exception.CustomException;
+import middle_point_search.backend.common.util.ResponseWriter;
+
+@Slf4j
+public class ExceptionHandlingFilter extends OncePerRequestFilter {
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+		FilterChain filterChain) throws ServletException, IOException {
+		try {
+			filterChain.doFilter(request, response);
+		} catch (CustomException e) {
+			log.warn("ExceptionHandlingFilter: {}", e.getMessage());
+
+			ErrorResponse errorResponse = ErrorResponse.from(e.getErrorCode());
+			ResponseWriter.writeResponse(response, errorResponse, e.getErrorCode().getHttpStatus());
+		} catch (Exception e) {
+			log.warn("ExceptionHandlingFilter: {}", e.getMessage());
+
+			ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+			ResponseWriter.writeResponse(response, errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+}
