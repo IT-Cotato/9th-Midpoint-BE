64009372af3a147679a97a8fa99648cb55059ece
hl3470 <hl3470@naver.com>
2024-06-11 22:29:44 +0900

    feat: DataResponse 구현

--- backend/src/main/java/middle_point_search/backend/common/dto/DataResponse.java
+++ backend/src/main/java/middle_point_search/backend/common/dto/DataResponse.java
@@ -1,9 +1,29 @@
 package middle_point_search.backend.common.dto;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 
-public class DataResponse extends BaseResponse{
-	protected DataResponse(boolean isSuccess, HttpStatusCode status) {
-		super(isSuccess, status);
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class DataResponse<T> extends BaseResponse {
+	private final T data;
+
+	@Builder
+	protected DataResponse(boolean isSuccess, HttpStatusCode statusCode, T data) {
+		super(isSuccess, statusCode);
+		this.data = data;
+	}
+
+	public static <T> DataResponse<T> from(T data) {
+		return DataResponse.<T>builder()
+			.isSuccess(true)
+			.statusCode(HttpStatus.OK)
+			.data(data)
+			.build();
 	}
 }
