e2e0aedd4e364b6e003c040b5029659a6f38b0f9
hl3470 <hl3470@naver.com>
2024-06-22 22:55:56 +0900

    refactor: Allargs -> NoArgs로 변경

--- backend/src/main/java/middle_point_search/backend/domains/place/dto/PlaceDTO.java
+++ backend/src/main/java/middle_point_search/backend/domains/place/dto/PlaceDTO.java
@@ -3,19 +3,21 @@ package middle_point_search.backend.domains.place.dto;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import middle_point_search.backend.domains.member.domain.Transport;
+import middle_point_search.backend.domains.place.domain.Place;
 
 public class PlaceDTO {
 
 	@Getter
-	@AllArgsConstructor(access = AccessLevel.PRIVATE)
+	@NoArgsConstructor(access = AccessLevel.PRIVATE)
 	public static class PlaceSaveRequest {
 
-		private final String siDo;
-		private final String siGunGu;
-		private final String roadNameAddress;
-		private final Double addressLat;
-		private final Double addressLong;
-		private final Transport transport;
+		private String siDo;
+		private String siGunGu;
+		private String roadNameAddress;
+		private Double addressLat;
+		private Double addressLong;
+		private Transport transport;
 	}
 }
