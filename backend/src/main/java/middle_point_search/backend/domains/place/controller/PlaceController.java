8ac9bee8e52e43eb2ef34a4ca1ce96ec00bd717c
hl3470 <hl3470@naver.com>
2024-06-22 22:56:50 +0900

    feat: PlacesFind 통신 구현

--- backend/src/main/java/middle_point_search/backend/domains/place/controller/PlaceController.java
+++ backend/src/main/java/middle_point_search/backend/domains/place/controller/PlaceController.java
@@ -1,6 +1,10 @@
 package middle_point_search.backend.domains.place.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,7 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import middle_point_search.backend.common.dto.BaseResponse;
+import middle_point_search.backend.common.dto.DataResponse;
 import middle_point_search.backend.domains.place.dto.PlaceDTO;
+import middle_point_search.backend.domains.place.dto.PlaceDTO.PlacesFindResponse;
 import middle_point_search.backend.domains.place.service.PlaceService;
 
 @RestController
@@ -20,8 +26,17 @@ public class PlaceController {
 
 	@PostMapping
 	public ResponseEntity<BaseResponse> placeSave(@RequestBody PlaceDTO.PlaceSaveRequest placeSaveRequest) {
+
 		placeService.savePlace(placeSaveRequest);
 
 		return ResponseEntity.ok(BaseResponse.ok());
+	}
+
+	@GetMapping("/{placeRoomId}")
+	public ResponseEntity<DataResponse<List<PlacesFindResponse>>> placesFind(@PathVariable("placeRoomId") String roomId) {
+
+		List<PlacesFindResponse> places = placeService.findPlaces(roomId);
+
+		return ResponseEntity.ok(DataResponse.from(places));
 	}
 }
