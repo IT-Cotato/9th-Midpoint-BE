c40dc23e343b980915d707ead6ed4150f352ae45
hl3470 <hl3470@naver.com>
2024-06-27 15:24:42 +0900

    feat: 중간지점 좌표 구하기 기능 구현
    그라함 스캔 알고리즘으로 구한 테두리 좌표들에 대해 중간 지점 좌표 구하기 기능을 구현하였다.

--- backend/src/main/java/middle_point_search/backend/domains/midPoint/util/MidPointUtilV1.java
+++ backend/src/main/java/middle_point_search/backend/domains/midPoint/util/MidPointUtilV1.java
@@ -22,10 +22,48 @@ public class MidPointUtilV1 implements MidPointUtil {
 
 		CoordinateDTO coordinate = calcMidPointCoordinate(coordinates);
 
+		return findMidPointsByCoordinate(coordinate);
+	}
+
+	// 주어진 중간 위치 좌표를 통해 추천 장소를 구하는 메서드
+	private List<AddressDTO> findMidPointsByCoordinate(CoordinateDTO coordinateDTO) {
 		return null;
 	}
 
+	// 여러 좌표의 무게 중심 구하는 메서드
 	private CoordinateDTO calcMidPointCoordinate(List<CoordinateDTO> coordinates) {
+
+		List<CoordinateDTO> borderCoordinates = grahamScan(coordinates);
+
+		double count = borderCoordinates.size();
+		double sumX = 0;
+		double sumY = 0;
+
+		for (CoordinateDTO coordinate : borderCoordinates) {
+			sumX += coordinate.getX();
+			sumY += coordinate.getY();
+		}
+
+		return new CoordinateDTO(sumX / count, sumY / count);
+	}
+
+	// 두 벡터의 외적을 통해 시계, 반시계 방향을 구하는 ccw 알고리즘
+	// CA X AB 이다.
+	// ccw => 1: 반시계, 0: 일직선, -1: 시계
+	private int ccw(CoordinateDTO a, CoordinateDTO b, CoordinateDTO c) {
+		double ccwR = (b.getX() - a.getX()) * (c.getY() - a.getX()) - (c.getX() - a.getX()) * (b.getY() - a.getY());
+
+		if (ccwR > 0) {
+			return 1;
+		} else if (ccwR < 0) {
+			return -1;
+		} else {
+			return 0;
+		}
+	}
+
+	// 그라함 스캔 알고리즘, 테두리 좌표들만 구하기
+	private List<CoordinateDTO> grahamScan(List<CoordinateDTO> coordinates) {
 		// 정렬의 기준이 될 좌표
 		CoordinateDTO standard = findStandardPoint(coordinates);
 
@@ -48,45 +86,15 @@ public class MidPointUtilV1 implements MidPointUtil {
 			}
 		});
 
-		List<CoordinateDTO> borderCoordinates = grahamScan(coordinates, standard);
-
-		for (CoordinateDTO c : borderCoordinates) {
-			log.info("x : {}, y : {}", c.getX(), c.getY());
-		}
-
-		return null;
-	}
-
-	private List<AddressDTO> findMidPointsByCoordinate(CoordinateDTO coordinateDTO) {
-		return null;
-	}
-
-	// 두 벡터의 외적을 통해 시계, 반시계 방향을 구하는 ccw 알고리즘
-	// CA X AB 이다.
-	// ccw => 1: 반시계, 0: 일직선, -1: 시계
-	private int ccw(CoordinateDTO a, CoordinateDTO b, CoordinateDTO c) {
-		double ccwR = (b.getX() - a.getX()) * (c.getY() - a.getX()) - (c.getX() - a.getX()) * (b.getY() - a.getY());
-
-		if (ccwR > 0) {
-			return 1;
-		} else if (ccwR < 0) {
-			return -1;
-		} else {
-			return 0;
-		}
-	}
-
-	// 그라함 스캔 알고리즘, 테두리 좌표들만 구하기
-	private List<CoordinateDTO> grahamScan(List<CoordinateDTO> coordinateDTOs, CoordinateDTO standard) {
 		Stack<CoordinateDTO> S = new Stack<>();
 		S.add(standard);
 
-		for (int i = 1; i < coordinateDTOs.size(); i++) {
+		for (int i = 1; i < coordinates.size(); i++) {
 			//시계 방향이면 제거 한다.
-			while (S.size() > 1 && ccw(S.get(S.size() - 2), S.get(S.size() - 1), coordinateDTOs.get(i)) <= 0) {
+			while (S.size() > 1 && ccw(S.get(S.size() - 2), S.get(S.size() - 1), coordinates.get(i)) <= 0) {
 				S.pop();
 			}
-			S.add(coordinateDTOs.get(i));
+			S.add(coordinates.get(i));
 		}
 
 		return S.stream().toList();
