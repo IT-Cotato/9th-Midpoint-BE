3280d20bb2721e390fe1fc599cffb5f526e383ac
hl3470 <hl3470@naver.com>
2024-06-29 16:11:34 +0900

    refactor: WebClientConf 파일 kakao와 Market에 대한 WebClient를 분리하였다.

--- backend/src/main/java/middle_point_search/backend/common/webClient/conf/WebClientConf.java
+++ backend/src/main/java/middle_point_search/backend/common/webClient/conf/WebClientConf.java
@@ -1,7 +1,5 @@
 package middle_point_search.backend.common.webClient.conf;
 
-import java.time.Duration;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
@@ -9,34 +7,43 @@ import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
 import io.netty.channel.ChannelOption;
+import lombok.RequiredArgsConstructor;
+import middle_point_search.backend.common.properties.KakaoProperties;
+import middle_point_search.backend.common.properties.MarketProperties;
 import reactor.netty.http.client.HttpClient;
-import reactor.netty.resources.ConnectionProvider;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebClientConf {
 
-	DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory();
+	private final MarketProperties marketProperties;
+	private final KakaoProperties kakaoProperties;
 
-	HttpClient httpClient = HttpClient.create()
+	private DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory();
+	private HttpClient httpClient = HttpClient.create()
 		.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000); // 5초
 
 	@Bean
-	public WebClient webClient() {
+	public WebClient webClientForMarket() {
 		factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY);
 		return WebClient.builder()
+			.baseUrl(marketProperties.getBaseUrl())
 			.uriBuilderFactory(factory)
-			.codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(2 * 1024 * 1024)) // 응답 payload가 클 경우 나는 에러 방지, 최대 2MB
+			.codecs(configurer -> configurer.defaultCodecs()
+				.maxInMemorySize(2 * 1024 * 1024)) // 응답 payload가 클 경우 나는 에러 방지, 최대 2MB
 			.clientConnector(new ReactorClientHttpConnector(httpClient))
 			.build();
 	}
 
 	@Bean
-	public ConnectionProvider connectionProvider() {
-		return ConnectionProvider.builder("http-pool")
-			.maxConnections(100)
-			.pendingAcquireTimeout(Duration.ofMillis(0))
-			.pendingAcquireMaxCount(-1)
-			.maxIdleTime(Duration.ofMillis(1000L))
+	public WebClient webClientForKakao() {
+		factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY);
+		return WebClient.builder()
+			.baseUrl(kakaoProperties.getBaseUrl())
+			.uriBuilderFactory(factory)
+			.codecs(configurer -> configurer.defaultCodecs()
+				.maxInMemorySize(2 * 1024 * 1024)) // 응답 payload가 클 경우 나는 에러 방지, 최대 2MB
+			.clientConnector(new ReactorClientHttpConnector(httpClient))
 			.build();
 	}
 }
