acbc1957848e29f44d473dcf9eda5b17cc31be86
hl3470 <hl3470@naver.com>
2024-06-29 16:12:36 +0900

    refactor: MarketProperties에 baseUrl 추가

--- backend/src/main/java/middle_point_search/backend/common/properties/MarketProperties.java
+++ backend/src/main/java/middle_point_search/backend/common/properties/MarketProperties.java
@@ -11,6 +11,7 @@ import lombok.Setter;
 public class MarketProperties {
 
 	private String key;
+	private String baseUrl;
 	private String marketApiCountUrl;
 	private String marketApiUrl;
 	private Integer marketApiRequestUnit;
