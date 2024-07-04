e62f7b0803fdd299cf5dd4695a3ceff04bfcb832
hl3470 <hl3470@naver.com>
2024-07-04 17:51:48 +0900

    refactor:roadNameAddress 파싱 예외 추가

--- backend/src/main/java/middle_point_search/backend/domains/market/dto/response/RecommendPlacesFindResponse.java
+++ backend/src/main/java/middle_point_search/backend/domains/market/dto/response/RecommendPlacesFindResponse.java
@@ -3,8 +3,11 @@ package middle_point_search.backend.domains.market.dto.response;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import middle_point_search.backend.common.exception.CustomException;
 import middle_point_search.backend.domains.market.dto.response.KakaoSearchResponse.Document;
 
+@Slf4j
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class RecommendPlacesFindResponse {
@@ -18,9 +21,17 @@ public class RecommendPlacesFindResponse {
 
 	public static RecommendPlacesFindResponse from(Document document) {
 		String[] splitAddress = document.getRoad_address_name().split(" ", 3);
-		String siDo = splitAddress[0];
-		String siGunGu = splitAddress[1];
-		String roadNameAddress= splitAddress[2];
+
+		String siDo = null;
+		String siGunGu = null;
+		String roadNameAddress = null;
+		if (splitAddress.length < 3) {
+			log.warn("RecommendPlacesFindResponse : {}의 Index가 범위를 초과했습니다.", document.getRoad_address_name());
+		} else {
+			siDo = splitAddress[0];
+			siGunGu = splitAddress[1];
+			roadNameAddress= splitAddress[2];
+		}
 
 		return new RecommendPlacesFindResponse(
 			document.getPlace_name(),
