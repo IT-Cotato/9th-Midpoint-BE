976923f0de411dd98fa0165ed52a63d6d78c12bf
hl3470 <hl3470@naver.com>
2024-07-07 18:14:32 +0900

    feat: room엔티티에 생성시간 저장하도록 구현

--- backend/src/main/java/middle_point_search/backend/BackendApplication.java
+++ backend/src/main/java/middle_point_search/backend/BackendApplication.java
@@ -2,8 +2,10 @@ package middle_point_search.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BackendApplication {
 
 	public static void main(String[] args) {
