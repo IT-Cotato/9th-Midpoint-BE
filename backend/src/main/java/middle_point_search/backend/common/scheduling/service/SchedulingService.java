35b46fd7a7c74a80fa2dca6390bdcf6845a60140
hl3470 <hl3470@naver.com>
2024-07-16 16:35:54 +0900

    feat: market api 자동화

--- backend/src/main/java/middle_point_search/backend/common/scheduling/service/SchedulingService.java
+++ backend/src/main/java/middle_point_search/backend/common/scheduling/service/SchedulingService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import middle_point_search.backend.domains.market.service.MarketService;
 import middle_point_search.backend.domains.room.repository.RoomRepository;
 
 @Service
@@ -16,11 +17,19 @@ import middle_point_search.backend.domains.room.repository.RoomRepository;
 public class SchedulingService {
 
 	private final RoomRepository roomRepository;
+	private final MarketService marketService;
 
 	@Transactional(readOnly = false)
 	@Async
 	@Scheduled(cron = "0 0 2 * * *") //오전 두시에 매번 초기화
 	public void autoDelete() {
 		roomRepository.deleteByCreatedAtBefore(LocalDateTime.now().minusDays(3));
+	}
+
+	@Transactional(readOnly = false)
+	@Async
+	@Scheduled(cron = "0 0 3 * * MON") //오전 두시에 매번 초기화
+	public void autoUpdate() {
+		marketService.updateMarket();
 	}
 }
