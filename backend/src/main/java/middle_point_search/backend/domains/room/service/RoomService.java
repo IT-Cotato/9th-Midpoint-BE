13bdbd381997eedf9369b3dd6f50a486d7e89ac0
hl3470 <hl3470@naver.com>
2024-07-16 15:41:11 +0900

    fix: if문 오류 수정

--- backend/src/main/java/middle_point_search/backend/domains/room/service/RoomService.java
+++ backend/src/main/java/middle_point_search/backend/domains/room/service/RoomService.java
@@ -8,7 +8,6 @@ import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import middle_point_search.backend.common.exception.CustomException;
 import middle_point_search.backend.common.exception.errorCode.CommonErrorCode;
-import middle_point_search.backend.common.exception.errorCode.UserErrorCode;
 import middle_point_search.backend.domains.room.domain.Room;
 import middle_point_search.backend.domains.room.domain.RoomType;
 import middle_point_search.backend.domains.room.dto.RoomDTO.RoomCreateResponse;
@@ -44,7 +43,7 @@ public class RoomService {
 		//1.방 type이 disable로 지정이 안되어 있을 경우에만 변경
 		//2.방 type과 다른 방타입으로 요청이 왔으면 에러 리턴
 		//3.그 외는 패스
-		if (roomType == RoomType.DISABLED) {
+		if (room.getRoomType() == RoomType.DISABLED) {
 			room.updateRoomType(roomType);
 		} else if (roomType != room.getRoomType()) {
 			throw new CustomException(CommonErrorCode.BAD_REQUEST);
