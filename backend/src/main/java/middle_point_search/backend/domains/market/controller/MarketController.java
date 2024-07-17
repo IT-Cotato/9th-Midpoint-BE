30b18b69a679e24001bc07a5f0d8ee83f6ef1664
hl3470 <hl3470@naver.com>
2024-07-17 23:12:46 +0900

    feat: MarketController swagger 적용

--- backend/src/main/java/middle_point_search/backend/domains/market/controller/MarketController.java
+++ backend/src/main/java/middle_point_search/backend/domains/market/controller/MarketController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -29,6 +30,10 @@ public class MarketController {
 	private final MarketService marketService;
 
 	@PostMapping("/market")
+	@Operation(
+		summary = "중간 장소 리스트 업데이트",
+		description = "중간 지점으로 선정될 장소를 업데이트 한다. AccessToken 필요(ADMIN 권한 필요.)"
+	)
 	public ResponseEntity<BaseResponse> marketUpdate() {
 		marketService.updateMarket();
 
@@ -38,6 +43,10 @@ public class MarketController {
 	}
 
 	@GetMapping("recommend-places")
+	@Operation(
+		summary = "중간 지점 근처 식당, 스터디, 카페 추천",
+		description = "주소, 카테고리, 페이지 정보를 이용해 장소를 추천한다. AccessToken 필요"
+	)
 	public ResponseEntity<DataResponse<Page<RecommendPlacesFindResponse>>> recommendPlacesFind(@Valid @ModelAttribute RecommendPlacesFindRequest request) {
 
 		Page<RecommendPlacesFindResponse> recommendPlaces = marketService.findRecommendPlaces(request);
