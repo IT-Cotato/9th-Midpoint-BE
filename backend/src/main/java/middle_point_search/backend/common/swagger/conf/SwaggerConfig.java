f6c1355d1c0c60b4c2e08143c560aa7a3f2e5ec4
hl3470 <hl3470@naver.com>
2024-07-17 18:10:22 +0900

    feat: swagger에 jwt기능 추가

--- backend/src/main/java/middle_point_search/backend/common/swagger/conf/SwaggerConfig.java
+++ backend/src/main/java/middle_point_search/backend/common/swagger/conf/SwaggerConfig.java
@@ -1,18 +1,30 @@
 package middle_point_search.backend.common.swagger.conf;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
 	@Bean
 	public OpenAPI openAPI() {
+
+		SecurityScheme securityScheme = new SecurityScheme()
+			.type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+			.in(SecurityScheme.In.HEADER).name("Authorization");
+		SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+
 		return new OpenAPI()
-			.components(new Components())
+			.components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+			.security(Arrays.asList(securityRequirement))
 			.info(apiInfo());
 	}
 
