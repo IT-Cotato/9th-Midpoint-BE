d7311c7620e2fa71ca3f4b7e5a19a7b7c56fdd40
hl3470 <hl3470@naver.com>
2024-07-18 18:07:16 +0900

    refactor: Role에 Admin 추가
    Role에 관리를 위한 Admin 권한을 추가하였다.

--- backend/src/main/java/middle_point_search/backend/BackendApplication.java
+++ backend/src/main/java/middle_point_search/backend/BackendApplication.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 @SpringBootApplication
 @EnableJpaAuditing
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "https://www.api.cotato-midpoint.site")})
 public class BackendApplication {
 
 	public static void main(String[] args) {
