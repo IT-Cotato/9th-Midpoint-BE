4d329557002c467446df67e19ef0f07a7ccb6757
hl3470 <hl3470@naver.com>
2024-07-22 13:45:00 +0900

    refactor: RefreshToken에 AccessToken 정보도 저장하도록 변경
    logOut시에 AccessToken으로 다시 로그인 되면 안되므로, 이 AccessToken은 Redis 서버에 저장하여, 이 AccessToken으로 접근하는 유저를 차단하기 위해서 따로 저장한다.

--- backend/src/main/java/middle_point_search/backend/common/redis/RefreshToken.java
+++ backend/src/main/java/middle_point_search/backend/common/redis/RefreshToken.java
@@ -1,26 +1,28 @@
 package middle_point_search.backend.common.redis;
 
-import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+import org.springframework.transaction.annotation.Transactional;
 
-@RedisHash(value = "refreshToken", timeToLive = 86400 )
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@RedisHash(value = "refreshToken", timeToLive =1209600)
 public class RefreshToken {
 
 	@Id
 	private String refreshToken;
 
+	@Indexed
+	private String accessToken;
+
 	private Long memberId;
 
-	public RefreshToken(final String refreshToken, final Long memberId) {
-		this.refreshToken = refreshToken;
-		this.memberId = memberId;
-	}
-
-	public String getRefreshToken() {
-		return refreshToken;
-	}
-
-	public Long getMemberId() {
-		return memberId;
+	@Transactional
+	public void updateAccessToken(String accessToken) {
+		this.accessToken = accessToken;
 	}
 }
