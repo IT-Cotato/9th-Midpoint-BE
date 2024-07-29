d1fdb1cef6dde7a78b8763a8ade57bed0d51d200
Ye jin Shin <108514552+shinyj0@users.noreply.github.com>
2024-07-30 00:52:42 +0900

    feat: 장소 투표하기 관련 dto에 validaion 추가

--- backend/src/main/java/middle_point_search/backend/domains/placeVoteRoom/dto/PlaceVoteRoomDTO.java
+++ backend/src/main/java/middle_point_search/backend/domains/placeVoteRoom/dto/PlaceVoteRoomDTO.java
@@ -73,6 +73,8 @@ public class PlaceVoteRoomDTO {
     @Getter
     @NoArgsConstructor(access = AccessLevel.PRIVATE)
     public static class PlaceVoteRequest{
+
+        @NotNull(message = "choicePlace은 비어 있을 수 없습니다.")
        private  Long choicePlace;
     }
 }
