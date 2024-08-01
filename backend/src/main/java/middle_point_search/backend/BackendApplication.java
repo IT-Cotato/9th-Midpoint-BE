7ac26ce680c6b3e28798fe41f3696bbc1f11029b
Ye jin Shin <108514552+shinyj0@users.noreply.github.com>
2024-08-01 18:32:38 +0900

    feat: 메서드 호출 및 실행시간 측정 로그 출력

--- backend/src/main/java/middle_point_search/backend/BackendApplication.java
+++ backend/src/main/java/middle_point_search/backend/BackendApplication.java
@@ -4,6 +4,7 @@ import java.util.TimeZone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
@@ -12,6 +13,7 @@ import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableAspectJAutoProxy
 @OpenAPIDefinition(servers = {@Server(url = "/", description = "https://www.api.cotato-midpoint.site")})
 public class BackendApplication {
 
