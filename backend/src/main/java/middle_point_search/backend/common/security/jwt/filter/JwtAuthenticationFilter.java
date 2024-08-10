7c2c95c15e25ea807841f96da9865297170dd06f
hl3470 <hl3470@naver.com>
2024-08-11 00:19:41 +0900

    fix: 시큐리티 에러 해결
    시큐리티 단에서 401에러가 뜨지 않고, 422에러가 뜨는 에러 해결했다.

--- backend/src/main/java/middle_point_search/backend/common/security/jwt/filter/JwtAuthenticationFilter.java
+++ backend/src/main/java/middle_point_search/backend/common/security/jwt/filter/JwtAuthenticationFilter.java
@@ -56,11 +56,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 		final String refreshToken = jwtTokenProvider.extractRefreshToken(request).orElse(null);
 		final String accessToken = jwtTokenProvider.extractAccessToken(request).orElse(null);
 		final String tokenRoomId = jwtTokenProvider.extractRoomId(accessToken).orElse(null);
-		final Room room = roomRepository.findByIdentityNumber(tokenRoomId)
-			.orElseThrow(() -> new CustomException(ROOM_NOT_FOUND));
-
-		final String nowRoomId = jwtTokenProvider.extractRoomId(request).orElse(null);
-		final RoomType nowRoomType = jwtTokenProvider.extractRoomType(request).orElse(null);
 
 		//1. access토큰이 존재하며, accessToken이 유효하면 인증
 		//2. access토큰이 존재하며, accesToken이 유효하지 않으면 에러 리턴
@@ -68,6 +63,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 		//4. refresh토큰이 존재하며, refreshToken이 유효하지 않으면 로그인 유도
 		//5. 그 외 모든 경우는 에러 리턴
 		if (accessToken != null && jwtTokenProvider.isTokenValid(accessToken)) {
+			final String nowRoomId = jwtTokenProvider.extractRoomId(request).orElse(null);
+			final RoomType nowRoomType = jwtTokenProvider.extractRoomType(request).orElse(null);
+
+			final Room room = roomRepository.findByIdentityNumber(tokenRoomId)
+				.orElseThrow(() -> new CustomException(ROOM_NOT_FOUND));
+
 			//토큰이 logout된 토큰인지 검사
 			if (jwtTokenProvider.isLogout(accessToken)) {
 				log.info("logout된 accessToken으로 인증 실패");
