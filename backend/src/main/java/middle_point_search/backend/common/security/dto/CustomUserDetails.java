cec1dc0ec0d6cab53697158652617c63f7b8315c
BOOK-U3FJG82JE7\USER <hl3470@naver.com>
2024-10-11 14:49:28 +0900

    refactor: 기존 시큐리티 코드 제거

--- backend/src/main/java/middle_point_search/backend/common/security/dto/CustomUserDetails.java
+++
@@ -1,7 +0,0 @@
-package middle_point_search.backend.common.security.dto;
-
-import org.springframework.security.core.userdetails.UserDetails;
-
-public interface CustomUserDetails extends UserDetails {
-	String getRoomId();
-}
