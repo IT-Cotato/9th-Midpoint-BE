e24f1c2f1a9883a3e53c042e2d8255327ac85339
BOOK-U3FJG82JE7\USER <hl3470@naver.com>
2024-10-12 16:52:33 +0900

    refactor: Exception 변경
    ErrorCode 대신 message와 Status를 가지도록 변경

--- backend/src/main/java/middle_point_search/backend/common/exception/CustomException.java
+++ backend/src/main/java/middle_point_search/backend/common/exception/CustomException.java
@@ -1,17 +1,26 @@
 package middle_point_search.backend.common.exception;
 
+import org.springframework.http.HttpStatus;
+
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import middle_point_search.backend.common.exception.errorCode.ErrorCode;
 
 @Getter
 public class CustomException extends RuntimeException {
 
-	private final ErrorCode errorCode;
+	private final HttpStatus httpStatus;
 
-	public CustomException(ErrorCode errorCode) {
-		super(errorCode.getMessage());
-		this.errorCode = errorCode;
+	public CustomException(HttpStatus httpStatus, String message) {
+		super(message);
+		this.httpStatus = httpStatus;
+	}
+
+	public static CustomException from(ErrorCode errorCode) {
+		return new CustomException(errorCode.getHttpStatus(), errorCode.getMessage());
+	}
+
+	public static CustomException of(HttpStatus httpStatus, String message) {
+		return new CustomException(httpStatus, message);
 	}
 }
 
