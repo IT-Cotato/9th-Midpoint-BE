cf0ecedbe4689c528c329fd2cfd9b60d0276e6f6
BOOK-U3FJG82JE7\USER <hl3470@naver.com>
2024-10-23 11:41:43 +0900

    refactor: SchedulingService의 불필요한 스케줄링 제거 및 오타 변경

--- backend/src/main/java/middle_point_search/backend/common/scheduling/service/SchedulingService.java
+++ backend/src/main/java/middle_point_search/backend/common/scheduling/service/SchedulingService.java
@@ -1,7 +1,5 @@
 package middle_point_search.backend.common.scheduling.service;
 
-import java.time.LocalDateTime;
-
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -9,26 +7,17 @@ import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import middle_point_search.backend.domains.market.service.MarketService;
-import middle_point_search.backend.domains.room.repository.RoomRepository;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class SchedulingService {
 
-	private final RoomRepository roomRepository;
 	private final MarketService marketService;
 
 	@Transactional(readOnly = false)
 	@Async
-	@Scheduled(cron = "0 0 2 * * *") //오전 두시에 매번 초기화
-	public void autoDelete() {
-		roomRepository.deleteByCreatedAtBefore(LocalDateTime.now().minusDays(3));
-	}
-
-	@Transactional(readOnly = false)
-	@Async
-	@Scheduled(cron = "0 0 3 * * MON") //오전 두시에 매번 초기화
+	@Scheduled(cron = "0 0 3 * * MON") //월요일 오전 세시에 초기화
 	public void autoUpdate() {
 		marketService.updateMarket();
 	}
