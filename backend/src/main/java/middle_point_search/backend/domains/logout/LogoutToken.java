c4fb9be700e67a8b0bebb3984cdeef1bf4d8f037
BOOK-U3FJG82JE7\USER <hl3470@naver.com>
2024-12-05 16:18:38 +0900

    refactor: 토큰 가져와지지 않는 에러 수정

--- backend/src/main/java/middle_point_search/backend/domains/logout/LogoutToken.java
+++ backend/src/main/java/middle_point_search/backend/domains/logout/LogoutToken.java
@@ -1,13 +1,17 @@
 package middle_point_search.backend.domains.logout;
 
+import static lombok.AccessLevel.*;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
 @RedisHash(value= "blacklist", timeToLive = 60*60)
 public class LogoutToken {
 
