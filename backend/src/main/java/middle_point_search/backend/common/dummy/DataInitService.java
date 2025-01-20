627da456be536d9dba3c70cd5bac668e42451885
BOOK-U3FJG82JE7\USER <hl3470@naver.com>
2025-01-20 18:16:49 +0900

    feat: room, memberRoom, place도 batch 저장되도록 변경

--- backend/src/main/java/middle_point_search/backend/common/dummy/DataInitService.java
+++ backend/src/main/java/middle_point_search/backend/common/dummy/DataInitService.java
@@ -7,20 +7,29 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import middle_point_search.backend.common.dummy.repository.JDBCRepository;
 import middle_point_search.backend.domains.member.domain.Member;
 import middle_point_search.backend.domains.member.domain.Role;
+import middle_point_search.backend.domains.member.repository.MemberRepository;
+import middle_point_search.backend.domains.memberRoom.domain.MemberRoom;
+import middle_point_search.backend.domains.place.domain.Place;
+import middle_point_search.backend.domains.room.domain.Room;
+import middle_point_search.backend.domains.room.repository.RoomRepository;
 
 @Slf4j
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class DataInitService {
 
 	private final PasswordEncoder passwordEncoder;
 	private final JDBCRepository JDBCRepository;
+	private final MemberRepository memberRepository;
+	private final RoomRepository roomRepository;
+
+	private final EntityManager entityManager;
 
 	@Transactional
 	public void initializeData() {
@@ -29,11 +38,11 @@ public class DataInitService {
 		// 시간 재기 추후 삭제
 		long startTime = System.currentTimeMillis();
 
-		String password = passwordEncoder.encode("1234");
 
 		// Members 생성
 		List<Member> members = new ArrayList<>();
-		for (int i = 1; i < 500000; i++) {
+		String password = passwordEncoder.encode("1234");
+		for (int i = 1; i <= 500000; i++) {
 			Member member = Member.createWithoutAddress(
 				"user" + i + "@test.com",
 				password,
@@ -41,7 +50,54 @@ public class DataInitService {
 				Role.USER);
 			members.add(member);
 		}
-		JDBCRepository.saveAll(members);
+		JDBCRepository.saveAllMembers(members);
+
+		members = memberRepository.findAll(); // 엔티티 조회
+
+		// Rooms 생성
+		List<Room> rooms = new ArrayList<>();
+		for (int i = 1; i <= 500000; i++) {
+			Room room = Room.builder()
+				.id(String.valueOf(i))
+				.name("room" + i)
+				.memo("memo" + i)
+				.build();
+			rooms.add(room);
+		}
+		JDBCRepository.saveAllRooms(rooms);
+
+		rooms = roomRepository.findAll(); //엔티티 조회
+
+		log.info("Room is managed by JPA: {}", entityManager.contains(rooms.get(0)));
+
+		// MemberRooms 생성
+		List<MemberRoom> memberRooms = new ArrayList<>();
+		for (int i = 1; i <= 500000; i++) {
+			MemberRoom memberRoom = MemberRoom.builder()
+				.member(members.get(i - 1))
+				.room(rooms.get(i - 1))
+				.build();
+			memberRooms.add(memberRoom);
+		}
+		JDBCRepository.saveAllMemberRooms(memberRooms);
+
+		// Places 생성
+		List<Place> places = new ArrayList<>();
+		for (int i = 1; i <= 500000; i++) {
+			Place place = Place.builder()
+				.siDo("siDo" + i)
+				.siGunGu("siGunGu" + i)
+				.roadNameAddress("roadNameAddress" + i)
+				.addressLatitude(37.0 + i * 0.0001)
+				.addressLongitude(127.0 + i * 0.0001)
+				.room(rooms.get(i - 1))
+				.member(members.get(i - 1))
+				.googlePlaceId("googlePlaceId" + i)
+				.build();
+			places.add(place);
+		}
+		entityManager.clear(); // room들이 변경감지되어 place를 저장하기 전에 clear
+		JDBCRepository.saveAllPlaces(places);
 
 		// 시간 재기, 추후 삭제
 		long endTime = System.currentTimeMillis();
--- backend/src/main/java/middle_point_search/backend/common/dummy/repository/JDBCRepository.java
+++ backend/src/main/java/middle_point_search/backend/common/dummy/repository/JDBCRepository.java
@@ -6,9 +6,11 @@ import java.util.List;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import middle_point_search.backend.domains.member.domain.Member;
+import middle_point_search.backend.domains.memberRoom.domain.MemberRoom;
+import middle_point_search.backend.domains.place.domain.Place;
+import middle_point_search.backend.domains.room.domain.Room;
 
 @Repository
 @RequiredArgsConstructor
@@ -16,8 +18,8 @@ public class JDBCRepository {
 
 	private final JdbcTemplate jdbcTemplate;
 
-	@Transactional
-	public void saveAll(List<Member> members) {
+	// 모든 멤버 bulk 저장
+	public void saveAllMembers(List<Member> members) {
 		String sql = "INSERT INTO member (email, pw, name, role, exist_address) " +
 			"VALUES (?, ?, ?, ?, ?)";
 
@@ -32,4 +34,55 @@ public class JDBCRepository {
 				ps.setBoolean(5, member.getExistAddress());
 			});
 	}
+
+	// 모든 방 bulk 저장
+	public void saveAllRooms(List<Room> rooms) {
+		String sql = "INSERT INTO room (room_id, room_name, room_memo) " +
+			"VALUES (?, ?, ?)";
+
+		jdbcTemplate.batchUpdate(sql,
+			rooms,
+			rooms.size(),
+			(PreparedStatement ps, Room room) -> {
+				ps.setString(1, room.getId());
+				ps.setString(2, room.getName());
+				ps.setString(3, room.getMemo());
+			});
+	}
+
+	// 모든 멤버 방 bulk 저장
+	public void saveAllMemberRooms(List<MemberRoom> memberRooms) {
+		String sql = "INSERT INTO member_room (member_id, room_id) " +
+			"VALUES (?, ?)";
+
+		jdbcTemplate.batchUpdate(sql,
+			memberRooms,
+			memberRooms.size(),
+			(PreparedStatement ps, MemberRoom memberRoom) -> {
+				ps.setLong(1, memberRoom.getMember().getId());
+				ps.setString(2, memberRoom.getRoom().getId());
+			});
+	}
+
+	// 모든 Place bulk 저장
+	public void saveAllPlaces(List<Place> places) {
+		String sql = "INSERT INTO place (si_do, si_gun_gu, road_name_address, address_latitude, address_longitude, room_id, member_id, google_place_id) " +
+			"VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+		jdbcTemplate.batchUpdate(sql,
+			places,
+			places.size(),
+			(PreparedStatement ps, Place place) -> {
+				ps.setString(1, place.getSiDo());
+				ps.setString(2, place.getSiGunGu());
+				ps.setString(3, place.getRoadNameAddress());
+				ps.setDouble(4, place.getAddressLatitude());
+				ps.setDouble(5, place.getAddressLongitude());
+				ps.setString(6, place.getRoom().getId());
+				ps.setLong(7, place.getMember().getId());
+				ps.setString(8, place.getGooglePlaceId());
+			});
+	}
+
+	// 모든 PLace bulk 저장
 }
