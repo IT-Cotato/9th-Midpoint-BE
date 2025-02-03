544dd6ce566a1615649b73fb3bd99e4d75143ac2
BOOK-U3FJG82JE7\USER <hl3470@naver.com>
2025-02-03 19:23:31 +0900

    refactor: swagger 문서를 위해 getter 추가

--- backend/src/main/java/middle_point_search/backend/domains/member/dto/request/LoginMemberRequest.java
+++ backend/src/main/java/middle_point_search/backend/domains/member/dto/request/LoginMemberRequest.java
@@ -1,7 +1,9 @@
 package middle_point_search.backend.domains.member.dto.request;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @AllArgsConstructor
 public class LoginMemberRequest {
 	private String email;
