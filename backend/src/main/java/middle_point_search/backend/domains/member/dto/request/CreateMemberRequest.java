565029136bc44ce2a252d001d7240bb95cff4d18
BOOK-U3FJG82JE7\USER <hl3470@naver.com>
2025-02-04 17:28:45 +0900

    refactor: CreateMemberRequest를 Record로 변경

--- backend/src/main/java/middle_point_search/backend/domains/member/dto/request/CreateMemberRequest.java
+++ backend/src/main/java/middle_point_search/backend/domains/member/dto/request/CreateMemberRequest.java
@@ -3,34 +3,16 @@ package middle_point_search.backend.domains.member.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Getter
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class CreateMemberRequest {
-
-	@NotBlank(message = "이름을 입력해주세요.")
-	private String name;
-
-	@Email(message = "이메일 형식이 아닙니다.")
-	private String email;
-
-	@NotBlank(message = "비밀번호를 입력해주세요.")
-	private String pw;
-
-	@NotNull(message = "주소가 있는 지 여부를 입력해주세요.")
-	private Boolean existAddress;
-
-	//주소
-	private String siDo;
-	private String siGunGu;
-	private String roadNameAddress;
-	private Double addressLatitude;
-	private Double addressLongitude;
-
-	//인증코드
-	@NotBlank(message = "인증코드를 입력해주세요.")
-	private String code;
-}
+public record CreateMemberRequest(
+	@NotBlank(message = "이름을 입력해주세요.") String name,
+	@Email(message = "이메일 형식이 아닙니다.") String email,
+	@NotBlank(message = "비밀번호를 입력해주세요.") String pw,
+	@NotNull(message = "주소가 있는 지 여부를 입력해주세요.") Boolean existAddress,
+	String siDo,
+	String siGunGu,
+	String roadNameAddress,
+	Double addressLatitude,
+	Double addressLongitude,
+	@NotBlank(message = "인증코드를 입력해주세요.") String code
+) {}
