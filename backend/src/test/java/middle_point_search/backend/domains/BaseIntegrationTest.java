35f06d3bb684841e84b4d38e73774019e1bea7fb
BOOK-U3FJG82JE7\USER <hl3470@naver.com>
2025-02-10 21:20:45 +0900

    fix: BaseIntegrationText 안넣은 오류 수정

---
+++ backend/src/test/java/middle_point_search/backend/domains/BaseIntegrationTest.java
@@ -0,0 +1,21 @@
+package middle_point_search.backend.domains;
+
+import org.junit.jupiter.api.Disabled;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@Disabled
+@AutoConfigureMockMvc
+@Transactional
+public class BaseIntegrationTest {
+	@Autowired
+	protected MockMvc mockMvc;
+	@Autowired
+	protected ObjectMapper objectMapper;
+}
