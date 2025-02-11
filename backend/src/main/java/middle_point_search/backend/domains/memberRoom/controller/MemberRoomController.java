33ada853636d9531cd323ab5719ba1489360d07f
BOOK-U3FJG82JE7\USER <hl3470@naver.com>
2025-02-11 18:34:56 +0900

    feat:: 방 나가기 api 구현

--- backend/src/main/java/middle_point_search/backend/domains/memberRoom/controller/MemberRoomController.java
+++ backend/src/main/java/middle_point_search/backend/domains/memberRoom/controller/MemberRoomController.java
@@ -3,6 +3,7 @@ package middle_point_search.backend.domains.memberRoom.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -132,4 +133,45 @@ public class MemberRoomController {
 
 		return ResponseEntity.ok(DataResponse.from(response));
 	}
+
+	@DeleteMapping("/rooms/{roomId}")
+	@Operation(
+		summary = "회원을 방에서 삭제",
+		description = "회원을 방에서 삭제합니다.",
+		responses = {
+			@ApiResponse(
+				responseCode = "200",
+				description = "성공"
+			),
+			@ApiResponse(
+				responseCode = "401",
+				description = "인증에 실패하였습니다.[C-101]",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+			),
+			@ApiResponse(
+				responseCode = "402",
+				description = "Access Token을 재발급해야합니다.[A-004]",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+			),
+			@ApiResponse(
+				responseCode = "404",
+				description = "존재하지 않는 방입니다.[R-201]",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+			),
+			@ApiResponse(
+				responseCode = "409",
+				description = "해당 방에 존재하지 않는 회원입니다.[MR-001]",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+			)
+		}
+	)
+	public ResponseEntity<DataResponse<Void>> deleteMemberFromRoom(@PathVariable("roomId") String roomId) {
+		Long memberId = memberLoader.getMemberId();
+
+		memberRoomService.deleteMemberFromRoom(memberId, roomId);
+
+		return ResponseEntity.ok(DataResponse.ok());
+	}
 }
+
+
