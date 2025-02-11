3386ed56124fe73247d572a030550c4c270fb658
BOOK-U3FJG82JE7\USER <hl3470@naver.com>
2025-02-11 20:04:23 +0900

    refactor: request에 validation 추가

--- backend/src/main/java/middle_point_search/backend/domains/member/dto/request/DeleteMemberRequest.java
+++ backend/src/main/java/middle_point_search/backend/domains/member/dto/request/DeleteMemberRequest.java
@@ -1,7 +1,9 @@
 package middle_point_search.backend.domains.member.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record DeleteMemberRequest(
-	String accessToken,
+	@NotBlank(message = "accessToken은 비어있으면 안됩니다.") String accessToken,
 	String withdrawalReason
 ) {
 }
