e824eeccd6477def7a2181225752742b53ef48de
BOOK-U3FJG82JE7\USER <hl3470@naver.com>
2025-02-16 11:57:29 +0900

    feat: SwaggerConfig에서 url 관리하도록 변경

---
+++ backend/src/main/java/middle_point_search/backend/common/properties/SwaggerProperties.java
@@ -0,0 +1,15 @@
+package middle_point_search.backend.common.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Component
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "swagger")
+public class SwaggerProperties {
+	private String url;
+}
