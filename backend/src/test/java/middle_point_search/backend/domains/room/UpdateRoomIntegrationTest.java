fcbd2100c39a3194bf13110dee070e57928248e9
BOOK-U3FJG82JE7\USER <hl3470@naver.com>
2025-02-18 17:45:19 +0900

    feat: 방 메모 수정 테스트 구현

--- backend/src/test/java/middle_point_search/backend/domains/room/UpdateRoomIntegrationTest.java
+++ backend/src/test/java/middle_point_search/backend/domains/room/UpdateRoomIntegrationTest.java
@@ -8,6 +8,7 @@ import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -21,6 +22,7 @@ import middle_point_search.backend.domains.member.domain.Member;
 import middle_point_search.backend.domains.member.repository.MemberRepository;
 import middle_point_search.backend.domains.memberRoom.service.MemberRoomService;
 import middle_point_search.backend.domains.room.domain.Room;
+import middle_point_search.backend.domains.room.dto.request.UpdateRoomMemoRequest;
 import middle_point_search.backend.domains.room.dto.request.UpdateRoomNameRequest;
 import middle_point_search.backend.domains.room.repository.RoomRepository;
 
@@ -64,89 +66,159 @@ public class UpdateRoomIntegrationTest extends BaseIntegrationTest {
 		memberRoomService.saveMemberToRoom(member, roomId);
 	}
 
-	@Test
-	@DisplayName("방이름 수정에 성공한다.")
-	public void 방이름수정성공() throws Exception {
-		// given
-		String newRoomName = "수정된 방 이름";
-		Room room = roomRepository.findById(roomId).get();
+	@Nested
+	@DisplayName("방 이름 수정")
+	class 방_이름_수정 {
 
-		UpdateRoomNameRequest request = new UpdateRoomNameRequest(newRoomName);
+		@Test
+		@DisplayName("방이름 수정에 성공한다.")
+		public void 방이름수정성공() throws Exception {
+			// given
+			String newRoomName = "수정된 방 이름";
+			Room room = roomRepository.findById(roomId).get();
 
-		// when
-		ResultActions resultActions = mockMvc.perform(patch("/api/rooms/{roomId}/name", roomId)
-			.contentType(MediaType.APPLICATION_JSON)
-			.content(objectMapper.writeValueAsString(request))
-			.header("Authorization", "Bearer " + member1AccessToken)
-			.accept(MediaType.APPLICATION_JSON));
+			UpdateRoomNameRequest request = new UpdateRoomNameRequest(newRoomName);
 
-		// then
-		resultActions.andExpect(status().isOk());
-		Assertions.assertEquals(newRoomName, room.getName());
+			// when
+			ResultActions resultActions = mockMvc.perform(patch("/api/rooms/{roomId}/name", roomId)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request))
+				.header("Authorization", "Bearer " + member1AccessToken)
+				.accept(MediaType.APPLICATION_JSON));
+
+			// then
+			resultActions.andExpect(status().isOk());
+			Assertions.assertEquals(newRoomName, room.getName());
+		}
+
+		private static Stream<String> provideInvalidName() {
+			return Stream.of(
+				null,
+				"",
+				"12345678901234567890123456789012345678901234567890123456789012345"
+			);
+		}
+
+		@ParameterizedTest
+		@MethodSource("provideInvalidName")
+		@DisplayName("방 이름이 형식에 맞지 않으면 방 이름 수정에 실패한다.")
+		public void 방이름형식에맞지않음_방이름수정실패(String name) throws Exception {
+			// given
+			UpdateRoomNameRequest request = new UpdateRoomNameRequest(name);
+
+			// when
+			ResultActions resultActions = mockMvc.perform(patch("/api/rooms/{roomId}/name", roomId)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request))
+				.header("Authorization", "Bearer " + member1AccessToken)
+				.accept(MediaType.APPLICATION_JSON));
+
+			// then
+			resultActions.andExpect(jsonPath("$.code").value("C-202"));
+		}
+
+		@Test
+		@DisplayName("방이 존재하지 않으면 방 이름 수정에 실패한다.")
+		public void 방이존재하지않음_방이름수정실패() throws Exception {
+			// given
+			String newRoomName = "수정된 방 이름";
+			String notExistRoomId = "notExistRoomId";
+
+			UpdateRoomNameRequest request = new UpdateRoomNameRequest(newRoomName);
+
+			// when
+			ResultActions resultActions = mockMvc.perform(patch("/api/rooms/{roomId}/name", notExistRoomId)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request))
+				.header("Authorization", "Bearer " + member1AccessToken)
+				.accept(MediaType.APPLICATION_JSON));
+
+			// then
+			resultActions.andExpect(jsonPath("$.code").value("R-201"));
+		}
+
+		@Test
+		@DisplayName("해당 방의 회원이 아니면 방 이름 수정에 실패한다.")
+		public void 해당방의회원이아님_방이름수정실패() throws Exception {
+			// given
+			String newRoomName = "수정된 방 이름";
+
+			UpdateRoomNameRequest request = new UpdateRoomNameRequest(newRoomName);
+
+			// when
+			ResultActions resultActions = mockMvc.perform(patch("/api/rooms/{roomId}/name", roomId)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request))
+				.header("Authorization", "Bearer " + member2AccessToken)
+				.accept(MediaType.APPLICATION_JSON));
+
+			// then
+			resultActions.andExpect(jsonPath("$.code").value("MR-003"));
+		}
 	}
 
-	private static Stream<String> provideInvalidName() {
-		return Stream.of(
-			null,
-			"",
-			"12345678901234567890123456789012345678901234567890123456789012345"
-		);
-	}
+	@Nested
+	@DisplayName("방 메모 수정")
+	class 방_메모_수정 {
 
-	@ParameterizedTest
-	@MethodSource("provideInvalidName")
-	@DisplayName("방 이름이 형식에 맞지 않으면 방 이름 수정에 실패한다.")
-	public void 방이름형식에맞지않음_방이름수정실패(String name) throws Exception {
-		// given
-		UpdateRoomNameRequest request = new UpdateRoomNameRequest(name);
+		@Test
+		@DisplayName("방메모 수정에 성공한다.")
+		public void 방메모수정성공() throws Exception {
+			// given
+			String newRoomMemo = "수정된 방 메모";
+			Room room = roomRepository.findById(roomId).get();
 
-		// when
-		ResultActions resultActions = mockMvc.perform(patch("/api/rooms/{roomId}/name", roomId)
-			.contentType(MediaType.APPLICATION_JSON)
-			.content(objectMapper.writeValueAsString(request))
-			.header("Authorization", "Bearer " + member1AccessToken)
-			.accept(MediaType.APPLICATION_JSON));
+			UpdateRoomMemoRequest request = new UpdateRoomMemoRequest(newRoomMemo);
 
-		// then
-		resultActions.andExpect(jsonPath("$.code").value("C-202"));
-	}
+			// when
+			ResultActions resultActions = mockMvc.perform(patch("/api/rooms/{roomId}/memo", roomId)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request))
+				.header("Authorization", "Bearer " + member1AccessToken)
+				.accept(MediaType.APPLICATION_JSON));
 
-	@Test
-	@DisplayName("방이 존재하지 않으면 방 이름 수정에 실패한다.")
-	public void 방이존재하지않음_방이름수정실패() throws Exception {
-		// given
-		String newRoomName = "수정된 방 이름";
-		String notExistRoomId = "notExistRoomId";
+			// then
+			resultActions.andExpect(status().isOk());
+			Assertions.assertEquals(newRoomMemo, room.getMemo());
+		}
 
-		UpdateRoomNameRequest request = new UpdateRoomNameRequest(newRoomName);
+		@Test
+		@DisplayName("방이 존재하지 않으면 방 메모 수정에 실패한다.")
+		public void 방이존재하지않음_방메모수정실패() throws Exception {
+			// given
+			String newRoomMemo = "수정된 방 메모";
+			String notExistRoomId = "notExistRoomId";
 
-		// when
-		ResultActions resultActions = mockMvc.perform(patch("/api/rooms/{roomId}/name", notExistRoomId)
-			.contentType(MediaType.APPLICATION_JSON)
-			.content(objectMapper.writeValueAsString(request))
-			.header("Authorization", "Bearer " + member1AccessToken)
-			.accept(MediaType.APPLICATION_JSON));
+			UpdateRoomMemoRequest request = new UpdateRoomMemoRequest(newRoomMemo);
 
-		// then
-		resultActions.andExpect(jsonPath("$.code").value("R-201"));
-	}
+			// when
+			ResultActions resultActions = mockMvc.perform(patch("/api/rooms/{roomId}/memo", notExistRoomId)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request))
+				.header("Authorization", "Bearer " + member1AccessToken)
+				.accept(MediaType.APPLICATION_JSON));
 
-	@Test
-	@DisplayName("해당 방의 회원이 아니면 방 이름 수정에 실패한다.")
-	public void 해당방의회원이아님_방이름수정실패() throws Exception {
-		// given
-		String newRoomName = "수정된 방 이름";
+			// then
+			resultActions.andExpect(jsonPath("$.code").value("R-201"));
+		}
 
-		UpdateRoomNameRequest request = new UpdateRoomNameRequest(newRoomName);
+		@Test
+		@DisplayName("해당 방의 회원이 아니면 방 메모 수정에 실패한다.")
+		public void 해당방의회원이아님_방메모수정실패() throws Exception {
+			// given
+			String newRoomMemo = "수정된 방 메모";
 
-		// when
-		ResultActions resultActions = mockMvc.perform(patch("/api/rooms/{roomId}/name", roomId)
-			.contentType(MediaType.APPLICATION_JSON)
-			.content(objectMapper.writeValueAsString(request))
-			.header("Authorization", "Bearer " + member2AccessToken)
-			.accept(MediaType.APPLICATION_JSON));
+			UpdateRoomMemoRequest request = new UpdateRoomMemoRequest(newRoomMemo);
 
-		// then
-		resultActions.andExpect(jsonPath("$.code").value("MR-003"));
+			// when
+			ResultActions resultActions = mockMvc.perform(patch("/api/rooms/{roomId}/memo", roomId)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request))
+				.header("Authorization", "Bearer " + member2AccessToken)
+				.accept(MediaType.APPLICATION_JSON));
+
+			// then
+			resultActions.andExpect(jsonPath("$.code").value("MR-003"));
+		}
 	}
 }
