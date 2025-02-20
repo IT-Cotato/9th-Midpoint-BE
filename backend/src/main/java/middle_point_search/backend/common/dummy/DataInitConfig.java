ce8a3c113c33da6c5fbfc20f8482d003d705e0ea
BOOK-U3FJG82JE7\USER <hl3470@naver.com>
2025-02-20 14:25:22 +0900

    feat: main 서버에서는 더미 테스트 기능 작동하지 않도록 구현

--- backend/src/main/java/middle_point_search/backend/common/dummy/DataInitConfig.java
+++ backend/src/main/java/middle_point_search/backend/common/dummy/DataInitConfig.java
@@ -3,10 +3,12 @@ package middle_point_search.backend.common.dummy;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import lombok.RequiredArgsConstructor;
 import middle_point_search.backend.domains.member.repository.MemberRepository;
 
+@Profile("!main")
 @Configuration
 @RequiredArgsConstructor
 public class DataInitConfig {
