a347785287d79878cebd00ad5edda448e50e7b42
BOOK-U3FJG82JE7\USER <hl3470@naver.com>
2025-03-06 19:47:46 +0900

    refactor: 테스트 코드 접근자 default 접근자로 변경

--- backend/src/test/java/middle_point_search/backend/domains/memberRoom/SaveMemberToRoomTest.java
+++ backend/src/test/java/middle_point_search/backend/domains/memberRoom/SaveMemberToRoomTest.java
@@ -34,7 +34,7 @@ public class SaveMemberToRoomTest extends BaseIntegrationTest {
 	private MemberRepository memberRepository;
 
 	@BeforeEach
-	public void setUp() throws Exception {
+	void setUp() throws Exception {
 		AccessTokenAndRefreshToken accessTokenAndRefreshToken = signupAndLoginMember(false);
 
 		memberEmail = NO_ADDRESS_MEMBER_EMAIL;
@@ -43,7 +43,7 @@ public class SaveMemberToRoomTest extends BaseIntegrationTest {
 
 	@Test
 	@DisplayName("방에 회원 저장에 성공한다.")
-	public void 방에회원저장성공() throws Exception {
+	void 방에회원저장성공() throws Exception {
 		// given
 		// 방 생성
 		String roomId = "roomId";
@@ -73,7 +73,7 @@ public class SaveMemberToRoomTest extends BaseIntegrationTest {
 
 	@Test
 	@DisplayName("방이 존재하지 않으면 R-201을 반환한다.")
-	public void 방이존재하지않으면_R_201을반환() throws Exception {
+	void 방이존재하지않으면_R_201을반환() throws Exception {
 		// given
 		String nonExistRoomId = "nonExistRoomId";
 
@@ -90,7 +90,7 @@ public class SaveMemberToRoomTest extends BaseIntegrationTest {
 
 	@Test
 	@DisplayName("해당 방에 이미 존재하는 회원이면 MR-002를 반환한다.")
-	public void 해당방에이미존재하는회원이면_MR_002를반환() throws Exception {
+	void 해당방에이미존재하는회원이면_MR_002를반환() throws Exception {
 		// given
 		// 방 생성
 		String roomId = "roomId";
