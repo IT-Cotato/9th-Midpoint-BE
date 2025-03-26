aba9202b426b27c321d535f46a2b1e8029f98d4c
BOOK-U3FJG82JE7\USER <hl3470@naver.com>
2025-03-26 13:09:01 +0900

    refactor: 중복 투표 확인 로직에서 중복된 코드를 메서드로 분리

--- backend/src/main/java/middle_point_search/backend/domains/placeVoteRoom/service/PlaceVoteService.java
+++ backend/src/main/java/middle_point_search/backend/domains/placeVoteRoom/service/PlaceVoteService.java
@@ -27,7 +27,6 @@ import middle_point_search.backend.domains.placeVoteRoom.repository.PlaceVoteCan
 @Transactional(readOnly = true)
 public class PlaceVoteService {
 
-	private final PlaceVoteMemberService placeVoteMemberService;
 	private final PlaceVoteRoomService placeVoteRoomService;
 	private final PlaceVoteCandidateRepository placeVoteCandidateRepository;
 	private final PlaceVoteCandidateMemberRepository placeVoteCandidateMemberRepository;
@@ -35,7 +34,7 @@ public class PlaceVoteService {
 
 	// 투표 처리
 	@Transactional(rollbackFor = {CustomException.class})
-	public void vote(Member member, String roomId, VotePlaceRequest voteRequest) {
+	public void votePlace(Member member, String roomId, VotePlaceRequest voteRequest) {
 		// 방에 대한 회원인지 확인
 		memberRoomValidateService.validateAuthorizedMember(member.getId(), roomId);
 
@@ -43,12 +42,7 @@ public class PlaceVoteService {
 			.orElseThrow(() -> CustomException.from(VOTE_ROOM_NOT_FOUND));
 
 		// 투표 했는지 확인
-		boolean alreadyVoted = placeVoteMemberService.existsByPlaceVote_PlaceVoteRoomAndMember(
-			placeVoteRoom,
-			member);
-		if (alreadyVoted) {
-			throw CustomException.from(ALREADY_VOTED);
-		}
+		validateAlreadyVoted(placeVoteRoom, member);
 
 		// 투표 후보 조회
 		long placeVoteId = voteRequest.getChoicePlace();
@@ -68,12 +62,7 @@ public class PlaceVoteService {
 		PlaceVoteRoom placeVoteRoom = placeVoteRoomService.findByRoomId(roomId)
 			.orElseThrow(() -> CustomException.from(VOTE_ROOM_NOT_FOUND));
 
-		// 투표 했는지 확인
-		boolean alreadyVoted = placeVoteCandidateMemberRepository.existsByPlaceVoteCandidate_PlaceVoteRoomAndMember(
-			placeVoteRoom, member);
-		if (!alreadyVoted) {
-			throw CustomException.from(VOTE_NOT_FOUND);
-		}
+		validateAlreadyVoted(placeVoteRoom, member);
 
 		// 기존 투표 삭제
 		placeVoteCandidateMemberRepository.deleteByPlaceVoteCandidate_PlaceVoteRoomAndMember(placeVoteRoom, member);
@@ -83,8 +72,15 @@ public class PlaceVoteService {
 		PlaceVoteCandidate candidate = placeVoteCandidateRepository.findById(placeVoteCandidateId)
 			.orElseThrow(() -> CustomException.from(CANDIDATE_NOT_FOUND));
 
-		PlaceVoteCandidateMember placeVoteCandidateMember = new PlaceVoteCandidateMember(candidate, member);
-		placeVoteCandidateMemberRepository.save(placeVoteCandidateMember);
+		placeVoteCandidateMemberRepository.save(new PlaceVoteCandidateMember(candidate, member));
+	}
+
+	// 투표 했는지 확인
+	private void validateAlreadyVoted(PlaceVoteRoom placeVoteRoom, Member member) {
+		if (placeVoteCandidateMemberRepository.existsByPlaceVoteCandidate_PlaceVoteRoomAndMember(
+			placeVoteRoom, member)) {
+			throw CustomException.from(ALREADY_VOTED);
+		}
 	}
 
 	// 내 투표 조회
